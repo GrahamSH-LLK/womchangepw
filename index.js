a44211f41307538fcbfce71be3b6cf158ef9443a
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-02-15 14:27:56 -0500

    v1

---
+++ index.js
@@ -0,0 +1,15 @@
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
+const prompts = require('prompts');
+ 
+(async () => {
+  const response = await prompts({
+    type: 'password',
+    name: 'pass',
+    message: 'What is your new pw?'
+  });
+ 
+  
+
+bcrypt.hash(response.pass, saltRounds, async function (err, hashedPassword) {console.log(hashedPassword)})
+})();
